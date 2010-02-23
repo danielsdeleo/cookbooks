eadb29eaaeafbdbc244b0f30b9b68bea636d09d0
Daniel DeLeo <dan@opscode.com>
2010-02-22 17:04:05 -0800

    Relicense notifications cookbook as apache2

--- notifications/metadata.rb
+++ notifications/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Dan DeLeo"
 maintainer_email  "dan@opscode.com"
-license           "Proprietary, DO NOT DISTRIBUTE OUTSIDE OPSCODE BITCHES"
-description       "Sets an at exit hook which forwards exceptions to a cloudant DB"
+license           "Apache 2.0"
+description       "Sets an at_exit hook which forwards exceptions to CouchDB and S3"
 version           "0.1"
 
 %w{ centos redhat fedora ubuntu debian }.each do |os|
--- notifications/recipes/default.rb
+++ notifications/recipes/default.rb
@@ -4,14 +4,24 @@
 # Cookbook Name:: notifications
 # Recipe:: default
 #
-# Copyright 2009, Opscode, Inc.
+# Copyright 2010, Opscode, Inc.
 #
-# All rights reserved, don't distribute.
-# PUT THIS FIRST IN THE RUN LIST!!
-# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ 
+###########################################################
 # This recipe does nothing. library does all the work.
-
+###########################################################
 
 # OMG GLOBALZ ARE EVIL EXCEPT THIS TIME THEY'RE NOT
 $HACKY_WAY_TO_PASS_NODE_TO_EXIT_HOOK=node
